12141dcabc071bf7901effc35263db5228ea3d24
Ty Kelly <tkelly8@apple.com>
2020-04-29 08:39:51 -0700

    add system tests for populations page [CH-1]

--- spec/system/get_population_spec.rb
+++ spec/system/get_population_spec.rb
@@ -7,8 +7,24 @@ RSpec.describe "Get population by year", type: :system do
     assert_selector "button[type=submit]"
   end
 
-  describe "When user enters a valid year" do
-    it "redirects to a results page"
-    it "shows a population figure"
+  context "when user enters a valid year" do
+    it "redirects to a results page" do
+      visit populations_path
+
+      fill_in "year", with: 1950
+      find("button[type='submit']").click
+
+      expect(current_path).to eq populations_by_year_path
+    end
+
+    it "shows a population figure" do
+      visit populations_path
+
+      fill_in "year", with: 1950
+      find("button[type='submit']").click
+
+      expect(page).to have_content "You requested the population for: 1950"
+      expect(page).to have_content "Population: 151325798"
+    end
   end
 end
