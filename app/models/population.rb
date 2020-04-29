56275c3c0dec6a3bfea1dfb457d65907a09c17d4
Ty Kelly <tkelly8@apple.com>
2020-04-29 09:19:33 -0700

    allow users to query populations for years that are between our known ones [CH-2]

--- app/models/population.rb
+++ app/models/population.rb
@@ -9,15 +9,53 @@ class Population < ApplicationRecord
     return 0 if year.to_i < min_year
     return Population.order(year: :asc).last.population if year.to_i > KNOWN_YEARS.last
     return Population.find_by(year: Date.new(year.to_i)).population if KNOWN_YEARS.include?(year.to_i)
+    population_for_unknown_year(year)
+  end
 
-    target_date = Date.new(year.to_i)
-    min_date = Date.new(min_year)
-    population = Population.where(year: min_date..target_date)
-                           .order(year: :asc)
-                           .last
-                           .population
+  private
 
-    population
+  def self.population_for_unknown_year(year)
+    population_of_previously_known_year = Population.find_by(year: Date.new(known_year_before_date(year))).population
+    population_increase_since_previously_known_year =
+      population_between_known_years_around_date(year) * date_as_percentage_between_known_years(year)
+
+    (population_of_previously_known_year + population_increase_since_previously_known_year).round
+  end
+
+  def self.date_as_percentage_between_known_years(year)
+    # This returns the percentage of the way our target year is between its closest surrounding known years.
+    # For example, if our year is 1955, this method would return 0.50 as it is halfway between its surrounding
+    # known years of 1950 and 1960.
+    (year - known_year_before_date(year)).to_f / 10
+  end
+
+  def self.population_between_known_years_around_date(year)
+    before_date, after_date = known_years_around_date(year)
+    Population.where(year: [Date.new(before_date), Date.new(after_date)])
+              .order(population: :desc)
+              .pluck(:population)
+              .reduce(:-)
+  end
+
+  def self.known_year_before_date(year)
+    known_years_around_date(year).first
+  end
+
+  def self.known_year_after_date(year)
+    known_years_around_date(year).last
+  end
+
+  def self.known_years_around_date(year)
+    known_year_before_date = nil
+    known_year_after_date = nil
+
+    KNOWN_YEARS.each do |known_year|
+      known_year_before_date = known_year if known_year < year.to_i
+      known_year_after_date = known_year if known_year > year.to_i
+      break if known_year_after_date
+    end
+
+    [known_year_before_date, known_year_after_date]
   end
 
 end
