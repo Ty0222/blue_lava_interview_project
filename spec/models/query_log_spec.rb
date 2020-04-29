036f6397c48ed015b0a8b20c387c71d816d00844
Ty Kelly <tkelly8@apple.com>
2020-04-29 14:04:19 -0700

    update tests to account for query log's new response source data [CH-7]

--- spec/models/query_log_spec.rb
+++ spec/models/query_log_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe QueryLog, type: :model do
   describe ".log" do
     it "logs query data" do
-      expect { QueryLog.log(query: "QUERY", response: "RESPONSE")
+      expect { QueryLog.log(query: "QUERY", response: "RESPONSE", response_source: "SOURCE")
       }.to change { QueryLog.count }.by 1
     end
   end
--- spec/views/query_logs/_query_logs.html.erb_spec.rb
+++ spec/views/query_logs/_query_logs.html.erb_spec.rb
@@ -2,14 +2,16 @@ require "rails_helper"
 
 RSpec.describe "query_logs/_query_logs.html.erb", type: :view do
   it "displays logged query request and response data" do
-    QueryLog.log(query: "QUERY", response: "RESPONSE")
+    QueryLog.log(query: "QUERY", response: "RESPONSE", response_source: "SOURCE")
 
     render partial: "query_logs/query_logs.html.erb", locals: { query_logs: QueryLog.all }
 
     expect(rendered).to have_content("Query Logs")
-    expect(rendered).to have_content("Query")
-    expect(rendered).to have_content("Response")
-    expect(rendered).to have_content("QUERY")
-    expect(rendered).to have_content("RESPONSE")
+    expect(rendered).to have_css("th", text: "Query")
+    expect(rendered).to have_css("th", text: "Response")
+    expect(rendered).to have_css("th", text: "Response Source")
+    expect(rendered).to have_css("td", text: "QUERY")
+    expect(rendered).to have_css("td", text: "RESPONSE")
+    expect(rendered).to have_css("td", text: "SOURCE")
   end
 end
