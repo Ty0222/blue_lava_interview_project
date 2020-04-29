44e882891c73af37928c7e0bbc4c578e51097a7c
Ty Kelly <tkelly8@apple.com>
2020-04-29 09:33:18 -0700

    update tests for Population.get to account for being able to fetch populations for unknown years [CH-2]

--- spec/models/population_spec.rb
+++ spec/models/population_spec.rb
@@ -9,25 +9,34 @@ RSpec.describe Population, type: :model do
   end
 
   describe ".get" do
-    it "should accept a year we know and return the correct population" do
-      expect(Population.get(1900)).to eq(76212168)
-      expect(Population.get(1990)).to eq(248709873)
+    context "when receiving a year we know" do
+      it "returns the correct population" do
+        expect(Population.get(1900)).to eq(76212168)
+        expect(Population.get(1990)).to eq(248709873)
+      end
     end
 
-    it "should accept a year we don't know and return the previous known population" do
-      expect(Population.get(1902)).to eq(76212168)
-      expect(Population.get(1908)).to eq(76212168)
+    context "when receiving a year we don't know but it is within our known years" do
+      it "returns a population based on a linear progression between our target year and the closest previously known year" do
+        expect(Population.get(1902)).to eq(79415434)
+        expect(Population.get(1908)).to eq(89025230)
+      end
     end
 
-    it "should accept a year that is before earliest known and return zero" do
-      expect(Population.get(1800)).to eq(0)
-      expect(Population.get(0)).to eq(0)
-      expect(Population.get(-1000)).to eq(0)
+
+    context "when receiving a year that is before our earliest known year" do
+      it "returns zero" do
+        expect(Population.get(1800)).to eq(0)
+        expect(Population.get(0)).to eq(0)
+        expect(Population.get(-1000)).to eq(0)
+      end
     end
 
-    it "should accept a year that is after latest known and return the last known population" do
-      expect(Population.get(2000)).to eq(248709873)
-      expect(Population.get(200000)).to eq(248709873)
+    context "when receiving a year that is after our latest known year" do
+      it "returns the last known population" do
+        expect(Population.get(2000)).to eq(248709873)
+        expect(Population.get(200000)).to eq(248709873)
+      end
     end
   end
 
