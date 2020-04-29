67d0d507f4a01012130719839739eb4e785a45e8
Ty Kelly <tkelly8@apple.com>
2020-04-29 14:09:54 -0700

    display user population requests data on query log view [CH-8]

--- app/controllers/query_logs_controller.rb
+++ app/controllers/query_logs_controller.rb
@@ -1,9 +1,13 @@
 class QueryLogsController < ApplicationController
-  helper_method :query_logs
+  helper_method :query_logs, :populations
 
   def index; end
 
   private
+
+  def populations
+    @populations = Population.includes(:requests)
+  end
 
   def query_logs
     @query_logs = QueryLog.all
