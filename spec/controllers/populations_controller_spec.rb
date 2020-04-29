e01536b98d48c064c7040f9863433aa6caeca8a8
Ty Kelly <tkelly8@apple.com>
2020-04-29 14:13:35 -0700

    add controller test for new user population request logging functionality [CH-8]

--- spec/controllers/populations_controller_spec.rb
+++ spec/controllers/populations_controller_spec.rb
@@ -40,8 +40,22 @@ RSpec.describe PopulationsController, type: :controller do
       }.to change{QueryLog.count}.by(1)
     end
 
+    it "logs data related to how many times a known population year is requested" do
+      known_year = 1900
+      unknown_year = 1901
+      population = Population.find_by(year: Date.new(known_year))
+
+      expect {
+        get :show, xhr: true, params: { year: known_year }
+      }.to change { population.reload.requests.size }.by 1
+
+      expect {
+        get :show, xhr: true, params: { year: unknown_year }
+      }.to change { population.reload.requests.size }.by 0
+    end
+
     context "when an error occurs within the controller" do
-      it "captures the error and logs the query request and error response" do
+      it "captures the error and logs the query request and error response only" do
        allow(Population).to receive(:get).and_raise StandardError
 
        get :show, xhr: true, params: { year: "1900" }
@@ -50,6 +64,18 @@ RSpec.describe PopulationsController, type: :controller do
 
        expect(log.query).to eq "1900"
        expect(log.response).to eq "StandardError"
+       expect(log.response_source).to eq nil
+      end
+
+      it "does not log any data related to how many times a known population year is requested" do
+        allow(Population).to receive(:get).and_raise StandardError
+
+        known_year = 1900
+        population = Population.find_by(year: Date.new(known_year))
+
+        get :show, xhr: true, params: { year: known_year }
+
+        expect(population.reload.requests.size).to eq 0
       end
     end
   end
